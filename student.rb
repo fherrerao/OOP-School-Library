bb6805fc0f080908562c3fe0fe1414fec2827d7f
fherrerao <fherrera0206@gmail.com>
2022-04-06 17:20:25 -0500

    Add classroom method to add students

--- student.rb
+++ student.rb
@@ -1,9 +1,16 @@
 require './person'
 
 class Student < Person
+  attr_accessor :classroom
+
   def initialize(classroom:, **options)
     super(**options)
     @classroom = classroom
+  end
+
+  def classroom=(classroom)
+    @classroom = classroom
+    classroom.students << self unless classroom.students.include?(self)
   end
 
   def play_hooky
