4146a8e282a54875f591aecf6adf4765ab32e0ac
fherrerao <fherrera0206@gmail.com>
2022-04-06 17:20:54 -0500

    Add create_rental method

--- person.rb
+++ person.rb
@@ -1,13 +1,17 @@
 require './nameable'
+require './rental'
 
 class Person < Nameable
   attr_accessor :id, :name, :age
+
+  attr_reader :rental
 
   def initialize(age:, name: 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rental = []
     super()
   end
 
@@ -17,6 +21,10 @@ class Person < Nameable
 
   def correct_name
     @name
+  end
+
+  def create_rental(date, book, person)
+    Rental.new(date, book, person)
   end
 
   private
